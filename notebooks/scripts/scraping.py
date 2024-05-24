6df4e5386060966a5df489f1cd36221303ec3cac
Leonard <lowehardmail@gmail.com>
2024-05-24 15:29:13 +0200

    PowerUp

--- notebooks/scripts/scraping.py
+++ notebooks/scripts/scraping.py
@@ -11,6 +11,9 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.alert import Alert
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.os_manager import ChromeType
 
 from .drive import get_files, update_file, get_file, create_file
 
@@ -56,14 +59,14 @@ def login(mail):
     
     options = Options()
     options.add_argument("--headless=new")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    # options.add_argument("--disable-gpu")
+    # options.add_argument("--no-sandbox")
+    # options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
     # options.add_argument("--disable-features=NetworkService")
     # options.add_argument("--window-size=700x865")
     # options.add_argument("--disable-features=VizDisplayCompositor")
     # options.add_argument('--ignore-certificate-errors')
-    driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()),options=options)
     url = "https://doc.morningstar.com/Fund.aspx?u=ALL#"
     driver.get(url)
     
