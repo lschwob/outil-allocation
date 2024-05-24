8d3cb54ad52b0b4989d4d37ea036c213fef870be
Leonard <lowehardmail@gmail.com>
2024-05-24 15:14:59 +0200

    PowerUp

--- notebooks/scripts/scraping.py
+++ notebooks/scripts/scraping.py
@@ -58,7 +58,7 @@ def login(mail):
     options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--disable-gpu")
+    # options.add_argument("--disable-gpu")
     # options.add_argument("--disable-features=NetworkService")
     # options.add_argument("--window-size=700x865")
     # options.add_argument("--disable-features=VizDisplayCompositor")
