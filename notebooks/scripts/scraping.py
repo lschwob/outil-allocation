cf423ec58b04d903dfd9520df04f9ca25cad07dd
Leonard <lowehardmail@gmail.com>
2024-05-24 15:47:39 +0200

    PowerUp

--- notebooks/scripts/scraping.py
+++ notebooks/scripts/scraping.py
@@ -12,6 +12,17 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.alert import Alert
 
+
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.firefox import GeckoDriverManager
+
+
 from .drive import get_files, update_file, get_file, create_file
 
 
@@ -54,16 +65,13 @@ def requirements(dic_file, progress, drive):
 
 def login(mail):
     
-    options = Options()
-    options.add_argument("--headless=new")
-    # options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--disable-gpu")
-    # options.add_argument("--disable-features=NetworkService")
-    options.add_argument("--window-size=700x865")
-    # options.add_argument("--disable-features=VizDisplayCompositor")
-    # options.add_argument('--ignore-certificate-errors')
-    driver = webdriver.Chrome(options=options)
+    firefoxOptions = Options()
+    firefoxOptions.add_argument("--headless")
+    service = Service(GeckoDriverManager().install())
+    driver = webdriver.Firefox(
+        options=firefoxOptions,
+        service=service,
+    )
     url = "https://doc.morningstar.com/Fund.aspx?u=ALL#"
     driver.get(url)
     
